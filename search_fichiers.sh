c465c000a9e12119c3d8353267bfa2a656eed73f
AmarChelouah <merwan.ch@icloud.com>
2019-11-24 18:17:30 +0100

    creation du fichiers search_fichiers.sh

---
+++ search_fichiers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#recuperation de la date d'aujourd'hui dans le bon format
+jour=$(date | cut -d "," -f1)
+echo "Bienvenue $USER, aujourdhui nous sommes le $jour"
+#recuperation du repertoire voulu en tant que variable
+read -p "Quel repertoire voulez vous utiliser aujourd'hui ? " directory
+echo "Vous avez choisi le repertoire : $directory "
+
+#listing de tous le contenu du repertoire en question demandé precedemment
+
+ls $directory
