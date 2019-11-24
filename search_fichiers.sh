4d9677c5b9f9c7a6a12932c6c6e06246df4c787d
AmarChelouah <merwan.ch@icloud.com>
2019-11-24 18:31:22 +0100

    ajout du pattern de filtrage dans le fichier search_fichiers.sh Q10

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -3,10 +3,12 @@
 #recuperation de la date d'aujourd'hui dans le bon format
 jour=$(date | cut -d "," -f1)
 echo "Bienvenue $USER, aujourdhui nous sommes le $jour"
+
 #recuperation du repertoire voulu en tant que variable
 read -p "Quel repertoire voulez vous utiliser aujourd'hui ? " directory
-echo "Vous avez choisi le repertoire : $directory "
+echo "Vous avez choisi le repertoire, bonne journee : $directory "
 
-#listing de tous le contenu du repertoire en question demandé precedemment et ajout d'un premier pattern fourni en parametre
 
-ls $directory/$1
+#Ajout du deuxieme pattern avec deux option, i pour enlever la case sensitivity,l pour que le script s'arrete dès le premier matching, il permet de chercher une chaine de caractere specifique dans un fichier
+
+grep -il $2 $directory/$1
