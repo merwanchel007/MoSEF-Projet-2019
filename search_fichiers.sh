c06edf52308f66a3b86e8bc94db5ee20129297d9
merwanchel007 <58145224+merwanchel007@users.noreply.github.com>
2019-11-29 18:13:44 +0100

    faute de frappe ligne 9 et correction

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -6,9 +6,9 @@ echo "Bienvenue $USER, aujourdhui nous sommes le $jour"
 
 #recuperation du repertoire voulu en tant que variable
 read -p "Quel repertoire voulez vous utiliser aujourd'hui ? " directory
-echo "Vous avez choisi le repertoire, bonne journee : $directory "
+echo "Vous avez choisi le repertoire $directory , bonne journee "
 
 
-#Ajout du deuxieme pattern avec deux option, i pour enlever la case sensitivity,l pour que le script s'arrete dès le premier matching, il permet de chercher une chaine de caractere specifique dans un fichier
+#Ajout du deuxieme pattern avec deux option, i pour enlever la case sensitivity,l pour que le script affiche le nom du fichier au lieu de la ligne dans le fichier, il permet de chercher une chaine de caractere specifique dans un fichier
 
 grep -il $2 $directory/$1
